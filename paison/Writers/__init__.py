6122845d0107adf379ddf2134beb287653fb05c5
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-02-13 19:46:15 -0800

    mathwriter

--- paison/Writers/__init__.py
+++ paison/Writers/__init__.py
@@ -23,4 +23,43 @@ class MathWriter(Writer):
         for i in range(length):
             expr += random.choice(symbols) + str(random.randrange(maxnum))
         self.expressions.append(expr)
-        
+
+    def varexpression(self, maxnum, length):
+        import random
+        expr = random.choice(self.variables)
+        symbols = "+ - * / // % **".split(" ")
+        for i in range(length):
+            if i%2 is 0:
+                expr += random.choice(symbols) + str(random.randrange(maxnum))
+            else:
+                expr += random.choice(symbols) + random.choice(self.variables)
+        self.expressions.append(expr)
+
+    def parenexpression(self, maxnum):
+        import random
+        symbols = "+ - * / // % **".split(" ")
+        expr = random.choice(self.variables) + random.choice(symbols)
+        expr += "(" + str(random.randrange(maxnum)) + random.choice(symbols) + str(random.randrange(maxnum)) + ")"
+        self.expressions.append(expr)
+
+     #creates a single mathematical expression as a mathematical method
+    @staticmethod
+    def singleexpression(self, variables, length, maxnum):
+        import random
+        expr = random.choice(variables)
+        symbols = "+ - * / // % **".split(" ")
+        for i in range(length):
+            if i%2 is 0:
+                expr += random.choice(symbols) + str(random.randrange(maxnum))
+            else:
+                expr += random.choice(symbols) + random.choice(variables)
+        return expr
+
+    #creates math expression in the form n symbol (m symbol c)
+    @staticmethod
+    def parenexpression(self, maxnum, variables):
+        import random
+        symbols = "+ - * / // % **".split(" ")
+        expr = random.choice(variables) + random.choice(symbols)
+        expr += "(" + str(random.randrange(maxnum)) + random.choice(symbols) + str(random.randrange(maxnum)) + ")"
+        return expr
